5bec79e2e1c1d5efe654834f6ef19df464ee68e6
Rolf Jonker <90272108+RolfJonker@users.noreply.github.com>
2025-03-27 17:46:10 +0100

    Het template voor de georeferentie werkgroep ingericht

--- js/config.js
+++ js/config.js
@@ -2,7 +2,11 @@ let respecConfig = {
   useLogo: true,
   useLabel: true,
   // title is verplicht! Neem hier de titel van het document op ----------------------
+<<<<<<< HEAD
   title: "[Georefereren GeoBIM]",
+=======
+  title: "[TODO: Neem titel op in config.js]",
+>>>>>>> 96264f1e2143cd0d39b9feae352f11c000a5d978
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
   specStatus: "wv",                   // Werkversie
   //specStatus: "cv",               // Consultatieversie
@@ -27,7 +31,11 @@ let respecConfig = {
   //licence: "cc0",                 // Public Domain Dedication
   licence: "cc-by",                 // Attribution, met bronvermelding
   //-- TODO shortName is verplicht! (komt in de URL: kies logische afkorting)--------------
+<<<<<<< HEAD
   shortName: "NL-GeoBIM-GeoRef",
+=======
+  shortName: "NL-ReSpec-GN-template",
+>>>>>>> 96264f1e2143cd0d39b9feae352f11c000a5d978
   
   //edDraftURI = De URI van de draft version. Deze wordt automatisch afgeleid van de github URI; maar kan hier overschreven worden. 
 	//edDraftURI: ["https://geonovum.github.io", "/", "shortName"],
@@ -46,7 +54,11 @@ let respecConfig = {
   editors:
     [
       {
+<<<<<<< HEAD
         name: "Rolf Jonker",
+=======
+        name: "voornaam achternaam",
+>>>>>>> 96264f1e2143cd0d39b9feae352f11c000a5d978
         company: "Geonovum",
         companyURL: "https://www.geonovum.nl",
       }
@@ -56,6 +68,7 @@ let respecConfig = {
   authors:
     [
       {
+<<<<<<< HEAD
         name: "Paul Strokap",
         company: "VORM",
         companyURL: "https://vorm.nl/",
@@ -66,6 +79,11 @@ let respecConfig = {
         name: "Hans Lammerts",
         company: "VORM",
         companyURL: "https://www.lammerts-engineering.nl/",
+=======
+        name: "voornaam achternaam",
+        company: "Geonovum",
+        companyURL: "https://www.geonovum.nl",
+>>>>>>> 96264f1e2143cd0d39b9feae352f11c000a5d978
       }
     ],
   // TODO: Vul de github URL in.
